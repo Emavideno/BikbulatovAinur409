65f01acc0f329c6c7cf0833990c233b3d0c3e3c8
Emavideno <166414249+Emavideno@users.noreply.github.com>
2024-11-22 21:28:22 +0300

    BubbleSort, Cube, Index of num

---
+++ MassivesHomework/NumInMassive.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassivesHomework
+{
+    internal class NumInMassiveClass
+    {
+        public static void NumInMassive(int[] array, int number)
+        {
+            int tempIEnd = array.Length - 1;
+            int tempIStart = 0;
+            int tempIMid = array.Length / 2;
+
+            while (tempIStart <= tempIEnd)
+            {
+                tempIMid = (tempIEnd + tempIStart) / 2;
+                Console.WriteLine();
+                if (array[tempIMid] == number)
+                {
+                    Console.WriteLine($"Ваше число под индексом: {tempIMid}");
+                    return;
+                }
+                else if (number > array[tempIMid])
+                    tempIStart = tempIMid + 1;
+                else
+                    tempIEnd = tempIMid - 1;
+            }
+
+            Console.WriteLine("Вашего числа нет(");
+        }
+    }
+}
---
+++ MassivesHomework/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassivesHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BeatifulOutput();
+            Console.WriteLine("Ответ на третью задачу");
+            Cube.RandomCube();
+
+            BeatifulOutput();
+            int[] array = InputArray();
+
+            BeatifulOutput();
+            Console.WriteLine("           Массив отсортирован");
+            int[] sortedArray = BubbleSortClass.BubbleSort(array);
+
+            BeatifulOutput();
+            Console.Write("Введите число, индекс которого вы хотите найти в массиве: ");
+            int number = int.Parse(Console.ReadLine());
+            NumInMassiveClass.NumInMassive(sortedArray, number);
+            BeatifulOutput();
+        }
+        public static void BeatifulOutput()
+        {
+            Console.WriteLine();
+            Console.WriteLine("=============================================");
+            Console.WriteLine();
+        }
+        public static int[] InputArray()
+        {
+            Console.WriteLine("             Определяем массив...");
+            Console.Write("Введите сколько элементов вы хотите в массиве: ");
+            int len = int.Parse(Console.ReadLine());
+            int[] array = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write("Введите целое число: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            return array;
+        }
+    }
+}
