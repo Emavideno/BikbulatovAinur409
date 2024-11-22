fd856724a3b709ac5a8a1310115e188f324e8bde
Emavideno <166414249+Emavideno@users.noreply.github.com>
2024-11-22 18:48:18 +0300

    Перевод в двоичную

---
+++ DoNumberBinary/DoNumberBinary/DoBinary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoNumberBinary
+{
+    internal class DoBinary
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое положительное число: ");
+            int N = int.Parse(Console.ReadLine());
+
+            if (N < 0)
+            {
+                Console.WriteLine("Число меньше нуля");
+                return;
+            }
+            string result = "";
+
+            while (N > 0)
+            {
+                result += N % 2;
+                N /= 2;
+            }
+
+            Console.WriteLine(result.Reverse().ToArray());
+        }
+    }
+}
